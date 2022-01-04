140ec7bbb66774446693e216ba346a6793a95e49
yusrilalbi <aa684011@gmail.com>
2022-01-04 17:04:05 +0700

    desain 50% upgrade Profil and Persentase

--- script.js
+++ script.js
@@ -15,24 +15,35 @@ function updateDataAPI() {
     url: 'https://indodax.com/api/summaries',
     success: function(data) {
       var row;
-      $('#coins').html(`<tr>
-                          <th>Pairs</th>
-                          <th>Harga</th> 
-                          <th>Beli</th> 
-                          <th>jual</th> 
-                          <th>Tertinggi 24h</th>
-                          <th>Terendah 24h</th>
+      $('#coins').html(`<tr style="cursor: pointer;">
+                          <th onclick="sortTable(0)">Pairs</th>
+                          <th onclick="sortTable(1)">Harga</th> 
+                          <th onclick="sortTable(2)">Beli</th> 
+                          <th onclick="sortTable(3)">jual</th> 
+                          <th onclick="sortTable(4)">Tertinggi 24h</th>
+                          <th onclick="sortTable(5)">Terendah 24h</th>
+                          <th onclick="sortTable(6)">Persentase</th>
                         </tr>`)
       for (var key in data.tickers) {
         if(key.includes(tes.value)){
+        var tx = data.prices_24h[key.slice(0,-4)+key.slice(-3)]/data.tickers[key].buy*100-100
+        tx = tx.toString().slice(0,4)
         row = `<tr id=${key} onclick='exfun(id)' style="cursor: pointer;">
               <td style="color: blue;">${key.toUpperCase()}</td>
               <td> ${data.tickers[key].last} </td>
               <td> ${data.tickers[key].buy} </td>
               <td> ${data.tickers[key].sell} </td>
               <td> ${data.tickers[key].high} </td>
-              <td> ${data.tickers[key].low} </td>
-            </tr>`
+              <td> ${data.tickers[key].low} </td>`
+        if(tx>0)
+        row = row + `<td style="color: green"><b> ${tx}% </b></td>
+        </tr>`
+        else if(tx==0)
+        row = row + `<td style="color: grey"><b> ${tx}% </b></td>
+        </tr>`
+        else
+        row = row + `<td style="color: red"><b> ${tx}% </b></td>
+        </tr>`
         $('#coins tr:last').after(row);
       }
     }
@@ -40,7 +51,7 @@ function updateDataAPI() {
       masukkanGambar2()
       clearTimeout(timer)
       $('#timer').html(reloadData)
-      setTimeout(updateDataAPI, reloadData * 1000)
+      setTimeout(updateDataAPI, 10000)
       updateTimer()
     },
     error: function(err) {
@@ -53,13 +64,14 @@ function findAPI(){
     url: 'https://indodax.com/api/summaries',
     success: function(data) {
       var row;
-      $('#coins').html(`<tr>
-                          <th>Pairs</th>
-                          <th>Harga</th> 
-                          <th>Beli</th> 
-                          <th>jual</th> 
-                          <th>Tertinggi 24h</th>
-                          <th>Terendah 24h</th>
+      $('#coins').html(`<tr style="cursor: pointer;">
+                          <th onclick="sortTable(0)">Pairs</th>
+                          <th onclick="sortTable(1)">Harga</th> 
+                          <th onclick="sortTable(2)">Beli</th> 
+                          <th onclick="sortTable(3)">jual</th> 
+                          <th onclick="sortTable(4)">Tertinggi 24h</th>
+                          <th onclick="sortTable(5)">Terendah 24h</th>
+                          <th onclick="sortTable(6)">Persentase</th>
                         </tr>`)
       for (var key in data.tickers) {
         if(key.includes(tes.value)){
@@ -70,6 +82,7 @@ function findAPI(){
               <td> ${data.tickers[key].sell} </td>
               <td> ${data.tickers[key].high} </td>
               <td> ${data.tickers[key].low} </td>
+              <td style="color: green"> 100% </td>
             </tr>`
         $('#coins tr:last').after(row);
       }
@@ -93,13 +106,13 @@ function generate2(){
   $.ajax({
     url: 'https://indodax.com/api/summaries',
     success: function(data) {
-      var c = 0;
       var d = 0;
       $("#passing").html(`
+                    <br>
                     <tr>
-                    <th>Tukar</th>
-                    <th>Harga</th>
-                    <th>Beli</th>
+                    <th>Pairs</th>
+                    <th>Harga Beli</th>
+                    <th>Harga Jual</th>
                     </tr>
       `)
       for (var key in data.tickers) {
@@ -145,7 +158,6 @@ updateDataAPI()
 
 var globalklik;
 function exfun(a) {
-  console.log("bro")
   globalklik = a
   generate2()
   masukkanGambar2()
@@ -157,3 +169,70 @@ $("#beli").on('click',function(){
   exfun(10)
 })
 cari.addEventListener('click',findAPI)
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("coins");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+function toFixed(value, precision) {
+  var precision = precision || 0,
+      power = Math.pow(10, precision),
+      absValue = Math.abs(Math.round(value * power)),
+      result = (value < 0 ? '-' : '') + String(Math.floor(absValue / power));
+
+  if (precision > 0) {
+      var fraction = String(absValue % power),
+          padding = new Array(Math.max(precision - fraction.length, 0) + 1).join('0');
+      result += '.' + padding + fraction;
+  }
+  return result;
+}
